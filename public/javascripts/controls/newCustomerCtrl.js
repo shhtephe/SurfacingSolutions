a455b3fdf63eb78ca5344dd3192b8aa67bba0318
Stephen Bailey <s.bassly@gmail.com>
2015-05-22 15:55:17 -0400

    workwork

--- public/javascripts/controls/newCustomerCtrl.js
+++ public/javascripts/controls/newCustomerCtrl.js
@@ -2,6 +2,20 @@
 
 app.controller('newCustomerCtrl', 
   ['$scope', '$http', function ($scope, $http) {
+    $scope.alerts = [
+    ];
+
+    $scope.addAlert = function(type, msg) {
+      $scope.alerts.push({
+        type: type,
+        msg: msg
+      });
+    };
+
+    $scope.closeAlert = function(index) {
+      $scope.alerts.splice(index, 1);
+    };
+
     $scope.newCustomer = function(a, b, c, d, e, f, g, h) {  
       var data = {
         "firstName":a, 
@@ -17,10 +31,12 @@ app.controller('newCustomerCtrl',
         success(function(data, status, headers, config) {
         // this callback will be called asynchronously
         // when the response is available
+        addAlert("success","New Customer has been saved!");
       }).
       error(function(data, status, headers, config) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
+        addAlert("danger","Error: New Customer could not be saved.");
       });
     }
 }]);

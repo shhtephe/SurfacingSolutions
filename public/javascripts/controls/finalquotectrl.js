7ef06276e00b55a7cb1e27195e47fa5253c5cd71
Stephen Bailey <s.bassly@gmail.com>
2015-09-24 16:57:30 -0400

    new final quote page

--- public/javascripts/controls/finalquotectrl.js
+++ public/javascripts/controls/finalquotectrl.js
@@ -4,3 +4,19 @@ app.controller('finalQuoteCtrl',
 	['$scope', '$state', function ($scope, $state) {
 
 	}]);
+
+app.filter('flattenRows', function() {
+    return function(counters) {
+      var flatten = [];
+      angular.forEach(counters, function(counter) {
+        addons = counter.addons;
+        flatten.push(counter);
+        if (addons) {
+          angular.forEach(addons, function(addon) {
+            flatten.push(angular.extend(addon, {addon: true}));
+          });
+        }
+      });
+      return flatten;
+    }
+  });

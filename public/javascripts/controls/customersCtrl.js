1abbc17d24d82c87169bd381b3c8e9d2bc8eed6e
Stephen Bailey <s.bassly@gmail.com>
2015-11-13 01:41:06 -0500

    fixed empty customers warning

--- public/javascripts/controls/customersCtrl.js
+++ public/javascripts/controls/customersCtrl.js
@@ -19,15 +19,14 @@
 
 	  	vm.alerts = [
 	  	];
-		$scope.init = function() {
-			console.log("This ran");
-			if (typeof vm.customers === "undefined" || vm.customers === "") {
-		    	vm.alerts.push({
-					type: "warning",
-			    	msg: "There are no customers. Head to Quote-->New Customers to add a new customer"
-			    });
-			};
+		
+		if (typeof vm.customers === "undefined" || vm.customers === "") {
+	    	vm.alerts.push({
+				type: "warning",
+		    	msg: "There are no customers. Head to Quote-->New Customers to add a new customer"
+		    });
 		};
+		
 		vm.closeAlert = function(index) {
 	    	vm.alerts.splice(index, 1);
 	  	};
